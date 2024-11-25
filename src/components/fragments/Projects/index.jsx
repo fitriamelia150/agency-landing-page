a486073435e7d19dfb7161f9f40331d32679bfe8
fitriamelia150 <fitriamelia000979@gmail.com>
2024-11-26 00:38:29 +0700

    responsive medium devices

--- src/components/fragments/Projects/index.jsx
+++ src/components/fragments/Projects/index.jsx
@@ -1,10 +1,15 @@
 const Projects = () => {
     return (
-        <div className="flex flex-row flex-wrap">
-            <img src="/images/mobile/image-gallery-milkbottles.jpg" alt="" className="w-1/2"/>
-            <img src="/images/mobile/image-gallery-orange.jpg" alt="" className="w-1/2"/>
-            <img src="/images/mobile/image-gallery-cone.jpg" alt="" className="w-1/2"/>
-            <img src="/images/mobile/image-gallery-sugar-cubes.jpg" alt="" className="w-1/2"/>
+        <div id="projects" className="flex flex-row flex-wrap">
+            <img src="/images/mobile/image-gallery-milkbottles.jpg" alt="" className="w-1/2 md:hidden"/>
+            <img src="/images/mobile/image-gallery-orange.jpg" alt="" className="w-1/2 md:hidden"/>
+            <img src="/images/mobile/image-gallery-cone.jpg" alt="" className="w-1/2 md:hidden"/>
+            <img src="/images/mobile/image-gallery-sugar-cubes.jpg" alt="" className="w-1/2 md:hidden"/>
+
+            <img src="/images/desktop/image-gallery-milkbottles.jpg" alt="" className="hidden md:block md:w-1/4"/>
+            <img src="/images/desktop/image-gallery-orange.jpg" alt="" className="hidden md:block md:w-1/4"/>
+            <img src="/images/desktop/image-gallery-cone.jpg" alt="" className="hidden md:block md:w-1/4"/>
+            <img src="/images/desktop/image-gallery-sugarcubes.jpg" alt="" className="hidden md:block md:w-1/4"/>
         </div>
     )
 }
